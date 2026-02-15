1375c58b8aee1cabe800ca0c7f02162f3b3fd6d0
RayanneBarboza <rayannebarbozaa@gmail.com>
2026-02-15 12:42:30 -0300

    Atualização exercício 1 - nome e idade

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,10 +1,18 @@
-package exercicios;
+import java.util.Scanner;
 
 public class Exercicio01 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Digite seu nome: ");
+        String name = scanner.nextLine();
+        System.out.println("Digite seu ano de nascimento: ");
+        int anoNascimento = scanner.nextInt();
+        int anoAtual = 2026;
 
-        System.out.println("Exercício 1 em construção");
+        int idade = anoAtual - anoNascimento;
+
+        System.out.println("Olá, " + name + "." + " Você tem " + idade + " anos!");
 
     }
 }
